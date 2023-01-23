a5c30957fe090cd4ec6bc3eab3ecbd4cf5ba612e
Seth Kerr <skerr@aggies.ncat.edu>
2023-01-23 16:02:02 -0700

    remove unnecessary includes in example

--- examples/EAAPMST3923A2_Test/EAAPMST3923A2_Test.ino
+++ examples/EAAPMST3923A2_Test/EAAPMST3923A2_Test.ino
@@ -1,13 +1,10 @@
-#include <Adafruit_I2CDevice.h>
-#include <Adafruit_I2CRegister.h>
-#include <Adafruit_BusIO_Register.h>
-#include <Wire.h>
 
 #include <EAAPMST3923A2.h>
 
+EAAPMST3923A2 test = EAAPMST3923A2();
+
 void setup()
 {
-   EAAPMST3923A2 test = EAAPMST3923A2();
    Serial.begin(115200);
    while (!Serial) { delay(10); 
    if (test.begin())
